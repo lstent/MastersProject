2185ab1ac6f8585c88b3b1440019e09a749486a4
lstent <lucstent@gmail.com>
2020-07-06 17:39:47 +0100

    okay actual project in the works edge detection- Check

--- app_class.py
+++ app_class.py
@@ -1,87 +1,27 @@
-import gym
 import numpy as np
-import matplotlib.pyplot as plt
-
-env = gym.make("MountainCar-v0")
-env.reset()
-
-LEARNING_RATE = 0.1
-DISCOUNT = 0.95
-EPISODES = 2000
-
-SHOW_EVERY = 500
-
-DISCRETE_0S_SIZE = [20] * len(env.observation_space.high)
-discrete_os_win_size = (env.observation_space.high-env.observation_space.low) / DISCRETE_0S_SIZE
-
-epsilon = 0.5
-START_EPSILON_DECAYING = 1
-END_EPSILON_DECAYING = EPISODES // 2
-
-epsilon_decay_value = epsilon/(END_EPSILON_DECAYING - START_EPSILON_DECAYING)
-
-q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_0S_SIZE + [env.action_space.n]))
-
-ep_rewards = []
-aggr_ep_rewards = {'ep': [], 'avg': [], 'min': [], 'max': []}
+from PIL import ImageGrab
+import cv2
+import time
 
 
-def get_discrete_state(state):
-    discrete_state = (state - env.observation_space.low) / discrete_os_win_size
-    return tuple(discrete_state.astype(np.int))
+def process_img(original_image):
+    # convert to grey
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    # edge detection
+    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    return processed_img
 
 
-for episode in range(EPISODES):
-    episode_reward = 0
-    if episode % SHOW_EVERY == 0:
-        print(episode)
-        render = True
-    else:
-        render = False
-
-    discrete_state = get_discrete_state(env.reset())
-    done = False
-    while not done:
-
-        if np.random.random() > epsilon:
-            action = np.argmax(q_table[discrete_state])
-        else:
-            action = np.random.randint(0, env.action_space.n)
-            
-        new_state, reward, done, _ = env.step(action)
-        episode_reward += reward
-        new_discrete_state = get_discrete_state(new_state)
-        if render:
-            env.render()
-        if not done:
-            max_future_q = np.max(q_table[new_discrete_state])
-            current_q = q_table[discrete_state + (action, )]
-            new_q = (1-LEARNING_RATE) * current_q + LEARNING_RATE* (reward + DISCOUNT * max_future_q)
-            q_table[discrete_state+(action, )] = new_q
-        elif new_state[0] >= env.goal_position:
-            print(f"We made it on episode {episode}")
-            q_table[discrete_state+(action, )] = 0
-
-        discrete_state = new_discrete_state
-
-    if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
-        episode -= epsilon_decay_value
-
-    ep_rewards.append(episode_reward)
-
-    if not episode % SHOW_EVERY:
-        average_reward = sum(ep_rewards[-SHOW_EVERY:])/len(ep_rewards[-SHOW_EVERY:])
-        aggr_ep_rewards['ep'].append(episode)
-        aggr_ep_rewards['avg'].append(average_reward)
-        aggr_ep_rewards['min'].append(min(ep_rewards[-SHOW_EVERY:]))
-        aggr_ep_rewards['max'].append(max(ep_rewards[-SHOW_EVERY:]))
-
-        print(f"Episode: {episode} avg: {average_reward} min: {min(ep_rewards[-SHOW_EVERY:])} max: {max(ep_rewards[-SHOW_EVERY:])}")
-
-env.close()
-
-plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['avg'], label="avg")
-plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['min'], label="min")
-plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['max'], label="max")
-plt.legend(loc=4)
-plt.show()
+#def main():
+last_time = time.time()
+while True:
+    # 800x60 windowed mode
+    screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
+    # print('loop took {} seconds' .format(time.time()-last_time))
+    last_time = time.time()
+    new_screen = process_img(screen)
+    cv2.imshow('window', new_screen)
+    # cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
